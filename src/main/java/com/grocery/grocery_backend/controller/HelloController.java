216903fc1d4e6cb581c0dcf994ff68bc4b272cf8
Raju Bandam <rajubandam387@gmail.com>
2026-06-15 08:17:48 -0400

    Initial commit

---
+++ src/main/java/com/grocery/grocery_backend/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.grocery.grocery_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Grocery App";
+	}
+
+}
